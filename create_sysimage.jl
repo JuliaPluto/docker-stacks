7b729b2f05976ca5d6f7a704a21b95862fec035d
Παναγιώτης Γεωργακόπουλος <pankgeorg@gmail.com>
2021-05-25 22:43:57 +0300

    Update versions and small precompile (#3)
    * fasterrrr
    
    * Update with sysimage
    
    * Fix permissions
    
    * Enable optimization
    
    * Update to Pluto 0.14.6

---
+++ create_sysimage.jl
@@ -0,0 +1,9 @@
+using Pkg
+Pkg.add(["Pluto", "PlutoUI", "PackageCompiler", "Plots"])
+
+using PackageCompiler
+create_sysimage([:Pluto, :PlutoUI, :Plots];
+    precompile_execution_file="warmup.jl",
+    replace_default=true,
+    cpu_target=PackageCompiler.default_app_cpu_target()
+)

57b6d9fa0002da5d0284b570e757b9bc66fa012e
Fons van der Plas <fons@plutojl.org>
2021-05-27 19:16:12 +0200

    dont sysimage plots and plutoui to prevent version issues

--- create_sysimage.jl
+++ create_sysimage.jl
@@ -1,8 +1,8 @@
 using Pkg
-Pkg.add(["Pluto", "PlutoUI", "PackageCompiler", "Plots"])
+Pkg.add(["Pluto", "PackageCompiler"])
 
 using PackageCompiler
-create_sysimage([:Pluto, :PlutoUI, :Plots];
+create_sysimage([:Pluto];
     precompile_execution_file="warmup.jl",
     replace_default=true,
     cpu_target=PackageCompiler.default_app_cpu_target()
